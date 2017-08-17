acce56a26267de8dff274bafaf9e1fa44ba11fae
jeffreyshen19 <jshen@andover.edu>
2017-08-17 11:26:14 -0700

    goodreads

--- src/main.js
+++ src/main.js
@@ -18,4 +18,22 @@ $(function(){
     typeSpeed: 30,
     startDelay: 1000
   });
+
+  $.get("https://jshen-labs.herokuapp.com/updates", function(data){
+    console.log(data);
+    for(var i = 0; i < 3; i++){
+      var imageURL = data[i].image_url;
+      var numberOfMs = 0;
+      for(var j = 0; j < imageURL.length; j++){
+        if(imageURL.charAt(j) == 'm'){
+          numberOfMs++;
+          if(numberOfMs == 3) {
+            imageURL = imageURL.substring(0, j) + "l" + imageURL.substring(j + 1);
+            j = imageURL.length;
+          }
+        }
+      }
+      $("#books-row").append("<a class = 'book' target = '_blank' href = '" + data[i].link + "'><img src = '" + imageURL + "'></a>");
+    }
+  });
 });
