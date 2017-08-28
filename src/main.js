8420c3b6c242240912101a0970e1c67e71e7a585
jeffreyshen19 <jshen@andover.edu>
2017-08-28 12:33:23 -0700

    fixed images

--- src/main.js
+++ src/main.js
@@ -21,18 +21,25 @@ $(function(){
 
   $.get("https://jshen-labs.herokuapp.com/updates", function(data){
     console.log(data);
-    for(var i = 0; i < 3; i++){
-      var imageURL = data[i].image_url;
-      var numberOfMs = 0;
-      for(var j = 0; j < imageURL.length; j++){
-        if(imageURL.charAt(j) == 'm'){
-          numberOfMs++;
-          if(numberOfMs == 3) {
-            imageURL = imageURL.substring(0, j) + "l" + imageURL.substring(j + 1);
-            j = imageURL.length;
+    for(var i = 0; i < 5; i++){
+      var imageURL;
+
+      if(!data[i].image_url.includes("nophoto")){
+        var numberOfMs = 0;
+        imageURL = data[i].image_url;
+        
+        for(var j = 0; j < imageURL.length; j++){
+          if(imageURL.charAt(j) == 'm'){
+            numberOfMs++;
+            if(numberOfMs == 3) {
+              imageURL = imageURL.substring(0, j) + "l" + imageURL.substring(j + 1);
+              j = imageURL.length;
+            }
           }
         }
       }
+      else imageURL = "http://covers.openlibrary.org/b/isbn/"+ data[i].isbn13 + "-L.jpg";
+
       $("#books-row").append("<a class = 'book' target = '_blank' href = '" + data[i].link + "'><img src = '" + imageURL + "'></a>");
     }
   });
